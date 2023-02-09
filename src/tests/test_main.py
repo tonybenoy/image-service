1375e33df640fe0df2f9cd20051b386d0eb29282
Tony Benoy <me@tonybenoy.com>
2023-02-10 00:15:44 +0200

    Add pytest

---
+++ src/tests/test_main.py
@@ -0,0 +1,9 @@
+from src.tests.utils import client
+
+
+def test_read_main():
+    response = client.get("/test")
+    assert response.status_code == 200
+    response_json = response.json()
+    assert response_json["result"] == "success"
+    assert response_json["msg"] == "It works!"
