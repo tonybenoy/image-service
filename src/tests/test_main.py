7f53a5818bff70fec7ac3273c4a62dce0c7d3e9d
Tony Benoy <me@tonybenoy.com>
2023-02-10 00:37:26 +0200

    Add tests

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -7,3 +7,18 @@ def test_read_main():
     response_json = response.json()
     assert response_json["result"] == "success"
     assert response_json["msg"] == "It works!"
+
+
+def test_index():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Upload an image" in response.content
+
+
+def test_upload_image():
+    response = client.post(
+        "/upload",
+        files={"image": open("src/tests/a.txt", "rb")},
+    )
+    assert response.status_code == 200
+    assert b"Invalid file type" in response.content
